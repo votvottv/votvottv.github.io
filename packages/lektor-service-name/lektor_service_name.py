016859954116bde72a514ab4a02005342c58878a
Silvio Rhatto <rhatto@torproject.org>
2022-10-04 12:44:55 -0300

    Feat: letkor_service_name: respect envvar.prefix configuration

--- packages/lektor-service-name/lektor_service_name.py
+++ packages/lektor-service-name/lektor_service_name.py
@@ -1,21 +1,13 @@
 # -*- coding: utf-8 -*-
 from lektor.pluginsystem import Plugin
-from os import environ
+from lektor_envvars      import LektorEnv
 
-# FIXME: use the same config as the envvars plugin
-DEFAULT_PREFIX = "LEKTOR_"
+class ServiceNameEnv(LektorEnv):
+    def service_name(self, alt):
+        default_name_var = self.prefix + 'SERVICE_NAME'
+        custom_name_var  = default_name_var + '_' + alt.replace('-', '_').upper()
 
-def service_name(alt):
-    default_name_var = DEFAULT_PREFIX + 'SERVICE_NAME'
-    custom_name_var  = default_name_var + '_' + alt.replace('-', '_').upper()
-
-    if custom_name_var in environ:
-        return environ[custom_name_var]
-
-    if default_name_var in environ:
-        return environ[default_name_var]
-
-    return ''
+        return self.env(custom_name_var, self.env(default_name_var, ''))
 
 
 class ServiceNamePlugin(Plugin):
@@ -23,4 +15,5 @@ class ServiceNamePlugin(Plugin):
     description = u'A plugin that gets a localized site service name from environment variables.'
 
     def on_process_template_context(self, context, **extra):
-        context['service_name'] = service_name
+        config                  = self.get_config()
+        context['service_name'] = ServiceNameEnv(config).service_name
